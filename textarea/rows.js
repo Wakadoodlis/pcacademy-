596ff3190f5256a40ecc2aa65960a07b8be272ee
Martynas <marcius117@gmail.com>
2018-11-11 15:36:53 +0200

    google style

--- textarea/rows.js
+++ textarea/rows.js
@@ -9,9 +9,12 @@ if (myTextarea !== "") {
 //tikrinam ar textarea perziangia min row skaiciu, pagal raidziu skaiciu ir uzdedam max row. Max row nuimam, jei text area sumazeja iki min row.
 function count(area) {
   // document.getElementById("count1").innerHTML = area.value.length;
-  if (area.value.length > 130) {
+  if (area.value.length > 138) {
     area.rows = maxRows;
+    document.getElementById("gvoice").style.padding = "72px";
+
   } else {
     area.rows = minRows;
+    document.getElementById("gvoice").style.padding = "14px";
   }
 }
