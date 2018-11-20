746a0f5f234ecc492156f3e9c001a76c7170e062
Martynas <marcius117@gmail.com>
2018-11-20 15:13:10 +0200

    loan culculating application

--- loanND-1120/src/loan Classes/consumLoan.ts
+++ loanND-1120/src/loan Classes/consumLoan.ts
@@ -8,7 +8,7 @@ export default class ConsumptLoan implements Loans {
     this.lAmount = lAmount;
     this.lTerm = lTerm;
   }
-
+  // culculating interest depending from item type. and also culculating monthly payment
   calculate(): number {
     // setting dropdown items from index
     let selector = document.getElementById("itemTypeC") as HTMLSelectElement;
@@ -51,48 +51,3 @@ export default class ConsumptLoan implements Loans {
     }
   }
 }
-
-// export default class ConsumptionLoan extends Loans {
-//   lValue = parseInt(
-//     (<HTMLInputElement>document.getElementById("loanAmountC")).value
-//   );
-//   lTerm = parseInt(
-//     (<HTMLInputElement>document.getElementById("loanTermC")).value
-//   );
-
-//   lInterest = 1.2;
-//   maxTerm = 30;
-
-//   checkFields() {
-//     this.lValue = parseInt(
-//       (<HTMLInputElement>document.getElementById("loanAmountC")).value
-//     );
-//     this.lTerm = parseInt(
-//       (<HTMLInputElement>document.getElementById("loanTermC")).value
-//     );
-//     this.maxTerm = 30;
-
-//     if (this.lTerm > this.maxTerm) {
-//       document.getElementById("form-wr").style.height = "420" + "px";
-//       document.getElementById("displayC").style.color = "red";
-//       document.getElementById(
-//         "displayC"
-//       ).innerHTML = `! Maximum term was exceeded !`;
-//     } else {
-//       document.getElementById("displayC").style.color = "#003C5A";
-//     }
-//   }
-//   getAmount() {
-//     this.lValue = parseInt(
-//       (<HTMLInputElement>document.getElementById("loanAmountC")).value
-//     );
-//     this.lTerm = parseInt(
-//       (<HTMLInputElement>document.getElementById("loanTermC")).value
-//     );
-//     this.lInterest = 1.2;
-//     let payment = (this.lValue * this.lInterest) / this.lTerm;
-//     document.getElementById("displayC").innerHTML = `${payment.toFixed(
-//       2
-//     )} €/month`;
-//   }
-// }
--- loanND-1120/src/loan Classes/housingLoan.ts
+++ loanND-1120/src/loan Classes/housingLoan.ts
@@ -18,7 +18,7 @@ export default class Houseloan implements Loans {
     let monthPay = (this.lAmount * interest) / (this.lTerm * 12);
     return parseInt(monthPay.toFixed(2));
   }
-
+  // culculating max loan amount
   calculate(): number {
     let termInMonth = this.lTerm * 12;
 
