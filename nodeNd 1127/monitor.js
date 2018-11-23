0415858d4153dad42ce3ed3b2e42e23772dd927f
Martynas <marcius117@gmail.com>
2018-11-23 16:43:48 +0200

    preparing for ND

---
+++ nodeNd 1127/monitor.js
@@ -0,0 +1,3 @@
+// Monitoring pc ress in half a sec
+
+// Paleidus skriptą monitor.js terminale kas puse sekundės vaizduoti kompiuterio resursus (Jie turi automatiškai atsinaujinti).
---
+++ nodeNd 1127/pcRess.js
@@ -0,0 +1,1 @@
+// Nuskaityti kompiuterio resursus (naudojama CPU, RAM procentais) ir atvaizduoti terminale.
