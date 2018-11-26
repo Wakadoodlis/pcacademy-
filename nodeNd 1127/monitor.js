a5cd934e5c6319eb9e17dca1c44432c4f2fa3a67
Martynas <marcius117@gmail.com>
2018-11-26 16:37:31 +0200

    cpu gpu usage

--- nodeNd 1127/monitor.js
+++ nodeNd 1127/monitor.js
@@ -1,3 +1,13 @@
 // Monitoring pc ress in half a sec
-
+const cpuGpuUsage = require("./pcRess");
 // Paleidus skriptą monitor.js terminale kas puse sekundės vaizduoti kompiuterio resursus (Jie turi automatiškai atsinaujinti).
+
+setInterval(function() {
+  var time = 0;
+  var maxINterval = 10;
+  time += 2;
+  if (maxINterval > time) {
+    clearInterval(this.setInterval);
+  }
+  cpuGpuUsage();
+}, 500);
--- nodeNd 1127/pcRess.js
+++ nodeNd 1127/pcRess.js
@@ -1,61 +1,13 @@
 // Nuskaityti kompiuterio resursus (naudojama CPU, RAM procentais) ir atvaizduoti terminale.
-// const stats = require("pc-stats");
 const os = require("os-utils");
-const tasklist = require("tasklist");
-// var os = require("os");
-// console.log(os.cpus());
 
-// var cpus = os.cpus();
-// for (var i = 0, len = cpus.length; i < len; i++) {
-//   console.log("CPU %s:", i);
-//   var cpu = cpus[i],
-//     total = 0;
+var cpuGpuUsage = function() {
+  os.cpuUsage(function(ress) {
+    let totalMem = os.totalmem();
+    let freeMem = os.freememPercentage();
+    let useMem = totalMem - freeMem;
+    return console.log(` Cpu usage ${ress} % and GPU: ${useMem} %`);
+  });
+};
 
-//   for (var type in cpu.times) {
-//     total += cpu.times[type];
-//   }
-
-//   for (type in cpu.times) {
-//     console.log("\t", type, Math.round((100 * cpu.times[type]) / total));
-//   }
-// }
-// stats()
-//   .then(statistics => {
-//     console.log(statistics);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-// SHOWS CPU USAGE
-os.cpuUsage(function(ress) {
-  console.log(`CPU Usage (%): ${ress}`);
-});
-
-// TOTATL MEMORY
-console.log(`total memory: ${os.totalmem()} `);
-
-tasklist().then(tasks => {
-  console.log(tasks.memUsage);
-  /*
-  [{
-      imageName: 'taskhostex.exe',
-      pid: 1820,
-      sessionName: 'Console',
-      sessionNumber: 1,
-      memUsage: 4415488,
-  }, …]
-  */
-});
-
-// os.cpuFree(function(ress) {
-//   console.log(`CPU free (%): ${ress}`);
-// });
-
-// os.platform(function(plat) {
-//   console.log(`platform name: ${plat}`);
-// });
-
-// os.cpuCount(function(count) {
-//   console.log(`cpu count: ${count}`);
-// });
+module.exports = cpuGpuUsage;
