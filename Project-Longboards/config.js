004e7c0f647af45c0556e43c17899e8566d6f49c
Martynas <marcius117@gmail.com>
2018-12-08 19:00:19 +0200

    users

--- Project-Longboards/config.js
+++ Project-Longboards/config.js
@@ -1,4 +1,6 @@
 module.exports = {
   PORT: 1234,
-  CONNECTION_STRING: "mongodb://localhost:27017/longboards"
+  CONNECTION_STRING: "mongodb://localhost:27017/longboards",
+  JWT_SECRET: "wakadakadoo",
+  JWT_HEADER_KEY: "wakaton"
 };
